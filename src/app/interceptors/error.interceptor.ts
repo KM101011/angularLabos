a86f824f6c2659c24acf85a9e607d2ef7d8e1b6f
Katja Medić <kmedic@fina.hr>
2025-07-07 13:29:50 +0200

    Error interceptor

---
+++ src/app/interceptors/error.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { NzMessageService } from 'ng-zorro-antd/message';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private message: NzMessageService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        this.message.error(error.error.message || "Server trenutno ne radi");
+        return throwError(() => error);
+      })
+    );
+  }
+}
